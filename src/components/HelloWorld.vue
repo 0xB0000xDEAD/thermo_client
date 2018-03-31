12c231985814ef2bae5a09366ce8129b435e857f
0XBO00OxDEAD <sergio@alpha.net>
2018-03-31 22:49:52 +0200

    added code to visualize the data from API added a map component (testing)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="hello">
+  <h1>{{ msg }}</h1>
+  <input v-model="filterArg" placeholder="filter by node name">
+  <button v-on:click="resetData" type="button" name="button">Delete Data</button>
+  <!-- <ul>
+    <li v-for="item of nodes">
+      {{ item }}
+    </li>
+  </ul> -->
+  <trend :data="nodes" :gradient="['#6fa8dc', '#42b983', '#2c3e50']" auto-draw smooth>
+  </trend>
+  <br>
+  <br>
+  <b-dropdown>
+            <button class="button is-primary" slot="trigger">
+                <span>Click me!</span>
+                <b-icon icon="menu-down"></b-icon>
+            </button>
+
+            <b-dropdown-item>Action</b-dropdown-item>
+            <b-dropdown-item>Another action</b-dropdown-item>
+            <b-dropdown-item>Something else</b-dropdown-item>
+        </b-dropdown>
+        <!-- <gmap></gmap> -->
+        
+</div>
 </template>
 
 <script>
+
+// import * as VueGoogleMaps from 'vue2-google-maps'
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: 'AIzaSyBJoqpzXmAlOuZ2e-eKP4wDe9YfLpnWhiY',
+//     libraries: 'places', // This is required if you use the Autocomplete plugin
+//     // OR: libraries: 'places,drawing'
+//     // OR: libraries: 'places,drawing,visualization'
+//     // (as you require)
+//   }
+// })
+
+
+// import buefy
+// import Vue from 'vue'
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
+import _ from "lodash";
+
+Vue.use(Buefy);
+import Vue from "vue";
+import Trend from "vuetrend";
+Vue.use(Trend);
+// import gmap from  './Map'
+// Vue.use(gmap)
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data: function() {
+    return {
+      nodes: [],
+      filterArg: ""
+    };
+  },
+  components: {
+    trend: Trend
+  },
   props: {
     msg: String
+  },
+  created: function() {
+    this.load();
+  },
+  watch: {
+    // whenever question changes, this function will run
+    filterArg: function () {
+      this.courtesy = 'Waiting for you to stop typing...'
+      this.load(this.filterArg)
+    }
+  },
+  methods: {
+     load: _.debounce(function(nodeId){
+       this.idToName(nodeId);
+      let myscope = this;
+    // console.log(myscope);
+    fetch("http://localhost:9000/api/thermonodes") // use get by id
+      .then(function(response) {      
+        return response.json();
+      }).then(function(data) {        
+        // console.log(typeof data);
+        console.log("this is the data structure: \n");
+        console.log(data);
+
+        myscope.nodes = data[0].temp;
+        
+
+
+        // for (let values of Object.values(data)) {
+        //   myscope.nodes.push(parseFloat(values.temp));
+        // }
+      })
+      .catch(function(err) {
+        console.log('there is a problem fetching the data', err);
+      })
+  }, 500),
+  idToName: function (nodeId) {
+    fetch("http://localhost:9000/api/thermonodes").then(function(response){
+      return(response.json());
+    }).then(function(data){
+      console.log(data);
+      
+    })
+    
+  },
+  resetData: function() {
+    fetch("delete Api", option).then(function(response) {
+      console.log(response.json());
+    })
   }
-}
+  /* getAnswer: _.debounce( // use debounce....plaese import lodash
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = 'Error! Could not reach the API. ' + error
+          })
+      },
+      // This is the number of milliseconds we wait for the
+      // user to stop typing.
+      500
+    ) */
+  } 
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -43,14 +146,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
---
+++ src/components/Map.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="map-container">
+  <gmap-map :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 500px; height: 300px"></gmap-map>
+
+</div>
+</template>
+
+<script>
+import * as VueGoogleMaps from 'vue2-google-maps';
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBJoqpzXmAlOuZ2e-eKP4wDe9YfLpnWhiY',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+});
+export default {
+  data: function() {
+    return {
+      //data placeholder
+    };
+  }
+};
+</script>
+
+<style>
+</style>
